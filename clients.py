4f6339c117eaeb9a0c8633ad6e72c9b9e51f0134
absitnikov <ab.sitnikov@mail.ru>
2022-09-10 17:43:25 +0500

    clients_db

--- clients.py
+++ clients.py
@@ -2,28 +2,23 @@ import psycopg2
 
 
 def create_db(conn):
-    user_db = """
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            first_name VARCHAR(100) NOT NULL,
-            last_name VARCHAR(100) NOT NULL,
-            email VARCHAR(100) NOT NULL
-        );
-    """
+    with conn.cursor() as cur:
+        cur.execute("""
+                        CREATE TABLE IF NOT EXISTS users(
+                        id SERIAL PRIMARY KEY,
+                        first_name VARCHAR(60) NOT NULL,
+                        last_name VARCHAR(60) NOT NULL,
+                        email VARCHAR(60) NOT NULL
+                        );
+                    """)
 
-    phone_db = """
-        CREATE TABLE IF NOT EXISTS phone (
-            id SERIAL PRIMARY KEY,
-            user_id INTEGER NOT NULL,
-            phone VARCHAR(100) NOT NULL,
-            FOREIGN KEY (user_id) REFERENCES users (id)
-        );
-    """
-
-    with conn.cursor() as cursor:
-        cursor.execute(user_db)
-        cursor.execute(phone_db)
-
+        cur.execute("""
+                        CREATE TABLE IF NOT EXISTS phone(
+                        id SERIAL PRIMARY KEY,
+                        phone VARCHAR(20),
+                        user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+                        );
+                    """)
         conn.commit()
 
 
@@ -54,6 +49,7 @@ def add_client(conn, first_name, last_name, email, phones=None):
         conn.commit()
         print(f'client info added: {first_name} {last_name}, {email} {phone}')
 
+
 def add_phone(conn, client_id, phone):
     with conn.cursor() as cur:
         cur.execute("""
@@ -64,20 +60,21 @@ def add_phone(conn, client_id, phone):
         conn.commit()
         print(f'new phone for {client_id} added: {phone}')
 
+
 def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
     with conn.cursor() as cur:
-        user_change = []
+        client_change = []
 
         if first_name:
-            user_change.append(f"first_name = '{first_name}'")
+            client_change.append(f"first_name = '{first_name}'")
         if last_name:
-            user_change.append(f"last_name = '{last_name}'")
+            client_change.append(f"last_name = '{last_name}'")
         if email:
-            user_change.append(f"email = '{email}'")
+            client_change.append(f"email = '{email}'")
 
         cur.execute(f"""
             UPDATE users
-            SET {','.join(user_change)}
+            SET {','.join(client_change)}
             WHERE id = {client_id}
         """)
 
@@ -91,9 +88,10 @@ def change_client(conn, client_id, first_name=None, last_name=None, email=None, 
                 cur.execute("""
                     INSERT INTO phone (user_id, phone)
                     VALUES (%s, %s)
-                """, (client_id, phones))
+                """, (client_id, phone))
 
         conn.commit()
+
 
 def delete_phone(conn, client_id, phone):
     with conn.cursor() as cur:
@@ -103,6 +101,7 @@ def delete_phone(conn, client_id, phone):
         """, (client_id, phone))
 
         conn.commit()
+
 
 def delete_client(conn, client_id):
     with conn.cursor() as cur:
@@ -118,38 +117,37 @@ def delete_client(conn, client_id):
 
         conn.commit()
 
+
 def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
     with conn.cursor() as cur:
-        find = []
 
-        if first_name:
-            find.append(f"first_name='{first_name}'")
-        if last_name:
-            find.append(f"last_name='{last_name}'")
-        if email:
-            find.append(f"email='{email}'")
-        if phone:
-            find.append(f"phone='{phone}'")
+        if phone is not None:
+            phone = str(phone)
+            cur.execute("""
+                SELECT users_id FROM phone
+                WHERE phone = %s;
+                        """, (phone,))
+            id_user = cur.fetchall()[0][0]
+            print(f'user ID: {id_user}')
 
-        sql = 'AND '.join(find)
-
-        cur.execute(f"""
-            SELECT users.id, first_name, last_name, email
-            FROM users
-            JOIN phone p ON users.id = p.user_id
-            WHERE {sql}
-        """)
-
-        return cur.fetchone()[0]
+        else:
+            cur.execute("""
+                SELECT id FROM users 
+                WHERE first_name = %s AND last_name = %s AND email = %s; 
+                        """, (first_name, last_name, email))
+            id_user = cur.fetchall()[0][0]
+            print(f'user ID: {id_user}')
 
 
 with psycopg2.connect(database="clients", user="postgres", password="postgres") as conn:
     create_db(conn)
-    add_client(conn, 'Ivan', 'Sidorov', 'not@mail.ru', ['+7343355'])
-    add_client(conn, 'Andrey', 'Petrov', 'not@mail.ru', ['+72525'])
-    user_find = find_client(conn, last_name='Petrov')
-    print(user_find)
-    add_phone(conn, '1', '+12345')
-    change_client(conn, 2, 'Ivan', 'Ivanov', 'yes@mail.ru', '4516')
-    delete_phone(conn, 2, '4516')
-    delete_client(conn, 1)
+    add_client(conn, 'Ivan', 'Sidorov', 'not@mail.ru', ['+7343566'])
+    add_client(conn, 'Andrey', 'Petrov', 'yes@mail.ru', ['+72525'])
+    add_client(conn, 'Alexander', 'Sidorov', 'data@mail.ru', ['12345'])
+    add_phone(conn, '2', '+72345')
+    change_client(conn, 3, 'Alexander', 'Belov', 'data2@mail.com', ['+712345'])
+    delete_phone(conn, 2, '+72525')
+    delete_client(conn, 3)
+    find_client(conn, 'Ivan', 'Sidorov', 'not@mail.ru')
+
+conn.close()
